417f279e66d36c7f44f5080edcabe2538ddb9b90
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-01-01 12:43:08 -0300

    Refeito

--- c++/2_metodo.cpp
+++ c++/2_metodo.cpp
@@ -1,33 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Personagem {
-    char nome[50];
-    int ataque;
-    int defesa;
-    int vida;
-    bool sobreviveu(int danoRecebido) {
-    
-    }    
-
-}Personagem;
-
-int main(){
-    Personagem personagem;
-
-    int dano;
-
-    scanf("%s", &personagem.nome);
-    scanf("%d", &personagem.ataque);
-    scanf("%d", &personagem.defesa);
-    scanf("%d", &personagem.vida);
-
-    scanf("%d", &dano);
-
-    if(personagem.sobreviveu(dano)){
-        printf("%s sobreviveu!!!", personagem.nome);
-    }else{
-        printf("%s morreu :(", personagem.nome);
+typedef struct personagem {
+    char nome[40];
+    int vida, defesa;
+    bool sobreviveu(int danoTomado) {
+        int danoReal = danoTomado - defesa;
+        if (danoReal < 0) 
+            danoReal = 0;
+        return (vida - danoReal) > 0;
     }
-}
+   
+}personagem;
 
+
+int main() {
+    personagem Personagem;
+    int dano;
+    scanf("%d %d %s %d", &Personagem.vida, &Personagem.defesa, Personagem.nome, &dano);
+    if (Personagem.sobreviveu(dano)) 
+        printf("Sobreviveu");
+    else 
+        printf("Morreu");
+}
+ 
