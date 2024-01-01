587db72c0bab4ac45aa63cf75ce9a5218f8fc319
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-01-01 12:33:32 -0300

    Kaua

--- c++/2_metodo.cpp
+++ c++/2_metodo.cpp
@@ -1,14 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct  aluno {
+typedef struct Personagem {
     char nome[50];
-    void falar() {
-        printf("%s\n", nome);
-    }
-};
+    int ataque;
+    int defesa;
+    int vida;
+    bool sobreviveu(int danoRecebido) {
+    
+    }    
 
-int main() {
-    aluno x = {"Kaua"}, y = {"auak"};
-    x.falar();
-    y.falar();
+}Personagem;
+
+int main(){
+    Personagem personagem;
+
+    int dano;
+
+    scanf("%s", &personagem.nome);
+    scanf("%d", &personagem.ataque);
+    scanf("%d", &personagem.defesa);
+    scanf("%d", &personagem.vida);
+
+    scanf("%d", &dano);
+
+    if(personagem.sobreviveu(dano)){
+        printf("%s sobreviveu!!!", personagem.nome);
+    }else{
+        printf("%s morreu :(", personagem.nome);
+    }
 }
+
